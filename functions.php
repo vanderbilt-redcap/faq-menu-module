d73d9c6682891007074d62d30dabdd6434d942eb
bascome <eva.bascompte.moragas@vumc.org>
2019-08-22 11:58:33 -0500

    Tests bug fix with image not displaying on iphone

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ function isUserExpiredOrSuspended($username,$field){
 function printFile($module,$edoc, $type){
     $file = "";
     if($edoc != ""){
-        $sql = "SELECT stored_name,doc_name,doc_size FROM redcap_edocs_metadata WHERE doc_id=" . $edoc;
+        $sql = "SELECT stored_name,doc_name,doc_size,mime_type FROM redcap_edocs_metadata WHERE doc_id=" . $edoc;
         $q = db_query($sql);
 
         if ($error = db_error()) {
@@ -39,10 +39,15 @@ function printFile($module,$edoc, $type){
 
         while ($row = db_fetch_assoc($q)) {
             $url = 'downloadFile.php?sname=' . $row['stored_name'] . '&file=' . urlencode($row['doc_name']);
+            $base64 = base64_encode(file_get_contents(EDOC_PATH.$row['stored_name']));
             if($type == "img"){
-                $file = '</br><div class="inside-panel-content"><img src="' . $module->getUrl($url,true) . '" style="display: block; margin: 0 auto;"></div>';
+                $file = '<br/><div class="inside-panel-content"><img src="' . $module->getUrl($url,true) . '" style="display: block; margin: 0 auto;"></div>';
+            }else if($type == "logo"){
+                $file = '<img src="data:'.$row['mime_type'].'base64,' . $base64. '" style="padding-bottom: 30px;width: 450px;">';
+            }else if($type == "imgpdf"){
+                $file = '<div style="max-width: 450px;height: 500px;"><img src="' . $module->getUrl($url,true) . '" style="display: block; margin: 0 auto;width:450px;height: 450px;"></div>';
             }else{
-                $file = '</br><div class="inside-panel-content"><a href="'.$module->getUrl($url,true).'" target="_blank"><span class="fa fa-file-o"></span> ' . $row['doc_name'] . '</a></div>';
+                $file = '<br/><div class="inside-panel-content"><a href="'.$module->getUrl($url,true).'" target="_blank"><span class="fa fa-file-o"></span> ' . $row['doc_name'] . '</a></div>';
             }
 
         }
